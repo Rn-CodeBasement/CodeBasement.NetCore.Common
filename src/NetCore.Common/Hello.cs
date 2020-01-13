bda8a57b6e9ab9c4d57f44b6f5fb15cfd31488b8
Richard Niemand <niemand.richard@gmail.com>
2020-01-13 16:07:56 -0700

    Starting project

---
+++ src/NetCore.Common/Hello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeBasement.NetCore.Common
+{
+  public static class Hello
+  {
+    public static void Speak()
+    {
+      Console.WriteLine("Hello World");
+    }
+  }
+}
