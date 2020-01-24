b90bc3f1ec6ee4dedddcf0505bba4ff04734a7c9
Richard Niemand <niemand.richard@gmail.com>
2020-01-23 20:09:35 -0700

    Porting JsonHelper

---
+++ src/NetCore.Common/Helpers/Interfaces/IJsonHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeBasement.NetCore.Common.Helpers.Interfaces
+{
+  public interface IJsonHelper
+  {
+    T DeserializeObject<T>(string value);
+    string SerializeObject(object value);
+    string SerializeObject(object value, bool formatted);
+  }
+}
---
+++ src/NetCore.Common/Helpers/JsonHelper.cs
@@ -0,0 +1,25 @@
+﻿using CodeBasement.NetCore.Common.Helpers.Interfaces;
+using Newtonsoft.Json;
+
+namespace CodeBasement.NetCore.Common.Helpers
+{
+  public class JsonHelper : IJsonHelper
+  {
+    public string SerializeObject(object value)
+    {
+      return SerializeObject(value, false);
+    }
+
+    public string SerializeObject(object value, bool formatted)
+    {
+      return formatted
+        ? JsonConvert.SerializeObject(value, Formatting.Indented)
+        : JsonConvert.SerializeObject(value);
+    }
+
+    public T DeserializeObject<T>(string value)
+    {
+      return JsonConvert.DeserializeObject<T>(value);
+    }
+  }
+}
